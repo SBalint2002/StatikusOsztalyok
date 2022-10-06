25fc20f95335f1361ad14739be4d190964850cef
Sárándi <sarandi.balint@gmail.com>
2022-10-06 09:03:59 +0200

    Main.java & Veletlen.java
    - Veletlen kereszt/vezetek nevek generalasa
    - Veletlen Teljes nev generalasa
    - Main-ben teszteles

--- src/hu/petrik/statikusosztalyok/Veletlen.java
+++ src/hu/petrik/statikusosztalyok/Veletlen.java
@@ -8,28 +8,66 @@ import java.util.Random;
 import java.util.Scanner;
 
 public final class Veletlen {
-    private Veletlen() {}
+    private Veletlen() {
+    }
 
     private static final Random rnd = new Random();
     private static List<String> vezNevek = feltolt("files/veznev.txt");
     private static List<String> ferfiKerNevek = feltolt("files/ferfikernev.txt");
     private static List<String> noiKerNevek = feltolt("files/ferfikernev.txt");
+
     private static List<String> feltolt(String fajlNev) {
         List<String> lista = new ArrayList<>();
         try {
             Scanner file = new Scanner(new File(fajlNev));
-            while (file.hasNext()){
+            while (file.hasNext()) {
                 String sor = file.nextLine();
                 lista.add(sor);
             }
             file.close();
-        }catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         return lista;
     }
 
-    public static int velEgesz(int min, int max){
+    public static int velEgesz(int min, int max) {
         return rnd.nextInt(max - min + 1) + min;
+    }
+
+    public static char velKarakter(char min, char max) {
+        return (char) velEgesz(min, max);
+    }
+
+    public static String velVezetekNev(){
+        return vezNevek.get(rnd.nextInt(vezNevek.size()));
+    }
+
+    /**
+     * Véletlen magyar keresztnév generálása
+     * @param nem A generált keresztnév neme. Férfi esetén true, Nő esetén false.
+     * return A generált keresztnév
+     */
+
+    public static String velKeresztnev(boolean nem){
+        String keresztNev;
+        if (nem){
+            keresztNev = velFerfiKeresztNev();
+        }else {
+            keresztNev = velNoiKeresztNev();
+        }
+        return keresztNev;
+    }
+
+    public static String velFerfiKeresztNev(){
+        return ferfiKerNevek.get(rnd.nextInt(ferfiKerNevek.size()));
+    }
+
+    public static String velNoiKeresztNev(){
+        return noiKerNevek.get(rnd.nextInt(noiKerNevek.size()));
+    }
+
+    public static  String velTeljesNev(boolean nem){
+        return velVezetekNev() + " " + velKeresztnev(nem);
     }
 }
